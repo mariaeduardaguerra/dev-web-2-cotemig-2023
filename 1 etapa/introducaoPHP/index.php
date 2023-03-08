ba515900666986526275b9bf21bdd18b086d0f28
Maria Eduarda Cândido Alves Guerra <99684296+mariaeduardaguerra@users.noreply.github.com>
2023-03-08 18:41:39 -0300

    Add files via upload

---
+++ 1 etapa/introducaoPHP/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="ISO-8859-1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>atividade introdução</title>
+</head>
+
+<body>
+
+    <h1>
+    <?php
+
+    function nome($n01 , $n02, $n03){
+        return "ol�, " . $n01 . $n02 . $n03;
+    }
+
+    $string01 = "lana ";
+    $string02 = "del ";
+    $string03 = "rey";
+
+    echo nome ($string01, $string02, $string03)
+
+    ?>
+    </h1>
+
+</body>
+</html>
